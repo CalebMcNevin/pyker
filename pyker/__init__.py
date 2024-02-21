a850efb420922b33a5e11b2a09bf66aecc98471e
Caleb McNevin <calebmcnevin@gmail.com>
2024-02-20 23:33:22 -0700

    Refactor: Move Card class to seperate 'card.py' file

--- pyker/__init__.py
+++ pyker/__init__.py
@@ -1,84 +1,13 @@
-from .enums import Suit, Rank, HandType
 from itertools import product, groupby, combinations
 from random import shuffle, seed
 from functools import total_ordering
 from collections import Counter
 from typing import Tuple
 
+from .enums import Suit, Rank, HandType
+from .card import Card
+
 seed(0)
-
-
-class Card(object):
-
-    def __init__(self, card_def: str | Tuple[Rank, Suit]):
-        match card_def:
-            case str():
-                self.rank, self.suit = Card.parse_card(card_def)
-            case tuple():
-                self.rank, self.suit = card_def
-            case _:
-                raise ValueError
-
-    def __eq__(self, other):
-        return self.rank == other.rank and self.suit == other.suit
-
-    def __lt__(self, other):
-        if (self.rank == Rank.ACE) ^ (other.rank == Rank.ACE):
-            return True
-        return self.rank.number < other.rank.number
-
-    def __gt__(self, other):
-        if (self.rank == Rank.ACE) ^ (other.rank == Rank.ACE):
-            return True
-        return self.rank.number > other.rank.number
-
-    def __le__(self, other):
-        return (self == other) or (self < other)
-
-    def __ge__(self, other):
-        return (self == other) or (self > other)
-
-    def __matmul__(self, other):
-        """Returns True if self and other are adjacent"""
-        if self == other:
-            return False
-        if abs(self.rank.number - other.rank.number) == 1:
-            return True
-        if self.rank == Rank.ACE or other.rank == Rank.ACE:
-            if self.rank == Rank.TWO or other.rank == Rank.TWO:
-                return True
-            if self.rank == Rank.KING or other.rank == Rank.KING:
-                return True
-        return False
-
-    def __lshift__(self, other):
-        if self.rank == Rank.ACE and other.rank == Rank.KING:
-            return False
-        if self.rank == Rank.TWO and other.rank == Rank.ACE:
-            return False
-        return self < other and self @ other
-
-    def __rshift__(self, other):
-        if self.rank == Rank.ACE and other.rank == Rank.TWO:
-            return False
-        if self.rank == Rank.KING and other.rank == Rank.ACE:
-            return False
-        return self > other and self @ other
-
-    @staticmethod
-    def parse_card(card_def: str):
-        if len(card_def.strip()) != 2:
-            raise ValueError(
-                'card_def must be two characters long. Ex: \'AD\'')
-        enums = list(Rank) + list(Suit)
-        mapping = {e.alias: e for e in enums}
-        els = [mapping[e] for e in card_def]
-        rank = list(filter(lambda e: type(e) == Rank, els))[0]
-        suit = list(filter(lambda e: type(e) == Suit, els))[0]
-        return (rank, suit)
-
-    def __str__(self):
-        return f"{self.rank.alias}{self.suit.symbol}"
 
 
 class Deck(object):
